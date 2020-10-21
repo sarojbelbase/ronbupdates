d21b9f4a588affa09f274186b3c48a2985d2cb4f
Siddharth Belbase <siddharthbelbase@gmail.com>
2020-10-21 16:08:02 +0545

    🚧 Work in progress...

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from telegram.ext import CommandHandler, Updater
-from ronb.bot.main import the_tweeter
+from telegram import Bot
+from ronb.tweet.show import fetch_tweets, logs
+from ronb.tweet.store import add_tweet
 from os import environ
 
 token = environ.get('BOT_TOKEN')
@@ -9,27 +10,35 @@ port = int(environ.get('PORT', 5000))
 name = "ronbupdates"
 host = "0.0.0.0"
 url = f"https://{name}.now.sh"
-updater = Updater(token, use_context=True)
+bot = Bot(token=token)
 
 
 app = Flask(__name__)
 
 
 @app.route(f'/{token}', methods=['POST'])
-def start():
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", the_tweeter))
-
-    updater.idle()
+def send_to_channel():
+    add_tweet()
+    count = int(logs().tweets_added)
+    the_list = fetch_tweets()[:count][::-1]
+    for the_tweet in the_list:
+        if the_tweet.image_url == "None":
+            bot.send_message(channel_name, text=the_tweet.tweet)
+        else:
+            bot.send_photo(
+                channel_name,
+                photo=the_tweet.image_url,
+                caption=the_tweet.tweet)
 
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def set_webhook():
-    updater.start_webhook(listen=host, port=port, url_path=token)
-    updater.bot.setWebhook(f'{url}/{token}')
+    bot.delete_webhook()
+    webhook = bot.set_webhook(f'{url}/{token}')
+    if webhook:
+        return "Webhook :)"
+    else:
+        return "Webhook :("
 
 
 @app.route('/')
